a9d8d5bb4184d2316faaef84149d966b2d855962
Douglas Vinicius Caldas Bonin <72112555+DViniciusBonin@users.noreply.github.com>
2020-10-23 12:05:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var collapse = document.getElementById('collapse')
+
+var menu = document.getElementById('menu-mobile')
+menu.style.display = 'none'
+function mostraMenu(){
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+      } else {
+        menu.style.display = "block";
+      }
+  }
+
